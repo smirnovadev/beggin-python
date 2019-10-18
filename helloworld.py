3cdf207f19f85bcbda286d72a183528e77ada49a
kiNgSresh <sreshtha.mehrotra@gmail.com>
2019-10-19 02:53:44 +0530

    I've imported three types of different libraries

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import pyfiglet
+import time
+import sys
+#Pyfiglet is a module using which you can print your text styles in a beautiful manner.
+#For using any module first we have to download it and then import it using import command.
+#The syntax to print Hello World using this will be:
+
+print(pyfiglet.figlet_format('Hello World !',font='3-d'))
+
+
+#Now to wait your program you have to import time module
+time.sleep(10)
+
+#To exit the program you have to import sys module
+
+sys.exit()
+
+#I hope you understood how to import different libraries in python and of course how to print in a cool way, sleep and exit  your program
